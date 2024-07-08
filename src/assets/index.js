1041abac1a665ebe3c556684314f60cc6037aaf5
David <goyaldavid55@gmail.com>
2024-07-08 11:24:57 +0530

    Added one more project

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,7 +5,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import browser from "./browser.png"
+import browser from "./browser.png";
 
 import css from "./tech/css.png";
 import git from "./tech/git.png";
@@ -17,43 +17,45 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import express from "./tech/express.webp"
-import scss from "./tech/scss.webp"
-import mui from "./tech/mui.png"
-import redis from "./tech/redis.webp"
-import firebase from "./tech/firebase.webp"
+import express from "./tech/express.webp";
+import scss from "./tech/scss.webp";
+import mui from "./tech/mui.png";
+import redis from "./tech/redis.webp";
+import firebase from "./tech/firebase.webp";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-import netflix from "./netflix.png"
+import netflix from "./netflix.png";
+import Hard from "./75Hard.png";
 
 export {
-  logo,
-  backend,
-  mobile,
-  web,
-  github,
-  menu,
-  close,
-  css,
-  browser,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  netflix,
-  carrent,
-  jobit,
-  express,
-  scss,
-  mui,
-  redis,
-  firebase,
-  tripguide,
+	logo,
+	backend,
+	mobile,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	browser,
+	git,
+	html,
+	javascript,
+	mongodb,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	netflix,
+	carrent,
+	jobit,
+	express,
+	scss,
+	mui,
+	redis,
+	firebase,
+	tripguide,
+	Hard,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,273 +1,323 @@
 import {
-    mobile,
-    backend,
-    web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    carrent,
-    jobit,
-    tripguide,
-    netflix,
-    express,
-    scss,
-    mui,
-    redis,
-    firebase
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "Web Developer",
-      icon: web,
-    },
-    {
-      title: "Frontend Developer",
-      icon: mobile,
-    },
-    {
-      title: "Backend Developer",
-      icon: backend,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "HTML",
-      icon: html,
-    },
-    {
-      name: "CSS",
-      icon: css,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "SCSS",
-      icon: scss,
-    },
-    {
-      name: "Material UI",
-      icon: mui,
-    },
-    {
-      name: "Firebase",
-      icon: firebase,
-    },
-    {
-      name: "Express JS",
-      icon: express,
-    },
-    {
-      name: "Redis",
-      icon: redis,
-    },
-  ];
-  
-  
-  const projects = [
-    {
-      name: "Chat App",
-      description:
-        "Web-based chat app that allows users to chat, send attachments realtime. Users can create their groups and manage it realtime also enabling users to get notifications.",
-      tags: [
-        {
-          name: "material-ui",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "chartjs",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "redux",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "rtk query",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "express",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "jsonwebtoken",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "socket.io",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "express",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "framer-motion",
-          color: "green-text-gradient",
-        },
-      ],
-      image: carrent,
-      source_code_link: "https://github.com/DavidGoyal/Chat-App-Frontend",
-      deployed_link:"https://chat-app-frontend-rho-three.vercel.app/",
-    },
-    {
-      name: "Shopio",
-      description:
-        "Full Stack Ecommerce Website enabling users to buy products and also having a special admin dashboard for admins to process orders, create new products, update existing products.",
-      tags: [
-        {
-          name: "typescript",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "chartjs",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "redux",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "rtk query",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "restapi",
-          color: "green-text-gradient",
-        },
-        {
-          name: "node-cache",
-          color: "green-text-gradient",
-        },
-        {
-          name: "express",
-          color: "green-text-gradient",
-        },
-        {
-          name: "mongodb",
-          color: "green-text-gradient",
-        },
-        {
-          name: "scss",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: jobit,
-      source_code_link: "https://github.com/DavidGoyal/MERN-ECommerce-FRONTEND.git",
-      deployed_link:"https://mern-e-commerce-frontend-seven.vercel.app/",
-    },
-    {
-      name: "XCrypto",
-      description:
-        "A crypto tracker website helping users to get realtime data about different crypto currencies and exchanges",
-      tags: [
-        {
-          name: "react",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "chakra-ui",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "framer-motion",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "chartjs",
-          color: "pink-text-gradient",
-        },
-        {
-          name: "react-icons",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/DavidGoyal/Crypto_App",
-      deployed_link:"https://crypto-app-tan-ten.vercel.app/"
-    },
-    {
-      name: "Netflix Clone",
-      description:
-        "UI clone of Netflix made using html and css",
-      tags: [
-        {
-          name: "html",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: netflix,
-      source_code_link: "https://github.com/DavidGoyal/Netflix-UI-Clone",
-    },
-  ];
-  
-  export { services, technologies, projects };
+	mobile,
+	backend,
+	web,
+	javascript,
+	typescript,
+	html,
+	css,
+	reactjs,
+	redux,
+	tailwind,
+	nodejs,
+	mongodb,
+	carrent,
+	jobit,
+	tripguide,
+	netflix,
+	express,
+	scss,
+	mui,
+	redis,
+	firebase,
+	Hard,
+} from "../assets";
+
+export const navLinks = [
+	{
+		id: "about",
+		title: "About",
+	},
+	{
+		id: "work",
+		title: "Work",
+	},
+	{
+		id: "contact",
+		title: "Contact",
+	},
+];
+
+const services = [
+	{
+		title: "Web Developer",
+		icon: web,
+	},
+	{
+		title: "Frontend Developer",
+		icon: mobile,
+	},
+	{
+		title: "Backend Developer",
+		icon: backend,
+	},
+];
+
+const technologies = [
+	{
+		name: "JavaScript",
+		icon: javascript,
+	},
+	{
+		name: "TypeScript",
+		icon: typescript,
+	},
+	{
+		name: "React JS",
+		icon: reactjs,
+	},
+	{
+		name: "Redux Toolkit",
+		icon: redux,
+	},
+	{
+		name: "Node JS",
+		icon: nodejs,
+	},
+	{
+		name: "MongoDB",
+		icon: mongodb,
+	},
+	{
+		name: "HTML",
+		icon: html,
+	},
+	{
+		name: "CSS",
+		icon: css,
+	},
+	{
+		name: "Tailwind CSS",
+		icon: tailwind,
+	},
+	{
+		name: "SCSS",
+		icon: scss,
+	},
+	{
+		name: "Material UI",
+		icon: mui,
+	},
+	{
+		name: "Firebase",
+		icon: firebase,
+	},
+	{
+		name: "Express JS",
+		icon: express,
+	},
+	{
+		name: "Redis",
+		icon: redis,
+	},
+];
+
+const projects = [
+	{
+		name: "SweatFuse",
+		description:
+			"A progress tracker website that allows users to track their progress for 75 days. Users need to uplaody a selfie daily and if they miss any day their progress will get resetted.",
+		tags: [
+			{
+				name: "typescript",
+				color: "green-text-gradient",
+			},
+			{
+				name: "material-ui",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "react",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "redux",
+				color: "green-text-gradient",
+			},
+			{
+				name: "rtk query",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "express",
+				color: "green-text-gradient",
+			},
+			{
+				name: "jsonwebtoken",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "express",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "restapi",
+				color: "green-text-gradient",
+			},
+		],
+		image: Hard,
+		source_code_link: "https://github.com/DavidGoyal/75Hard-Frontend.git",
+		deployed_link: "https://75-hard-frontend.vercel.app",
+	},
+	{
+		name: "Chat App",
+		description:
+			"Web-based chat app that allows users to chat, send attachments realtime. Users can create their groups and manage it realtime also enabling users to get notifications.",
+		tags: [
+			{
+				name: "material-ui",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "chartjs",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "react",
+				color: "green-text-gradient",
+			},
+			{
+				name: "redux",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "rtk query",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "green-text-gradient",
+			},
+			{
+				name: "express",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "jsonwebtoken",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "socket.io",
+				color: "green-text-gradient",
+			},
+			{
+				name: "express",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "restapi",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "framer-motion",
+				color: "green-text-gradient",
+			},
+		],
+		image: carrent,
+		source_code_link: "https://github.com/DavidGoyal/Chat-App-Frontend",
+		deployed_link: "https://chat-app-frontend-rho-three.vercel.app",
+	},
+	{
+		name: "Shopio",
+		description:
+			"Full Stack Ecommerce Website enabling users to buy products and also having a special admin dashboard for admins to process orders, create new products, update existing products.",
+		tags: [
+			{
+				name: "typescript",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "chartjs",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "react",
+				color: "green-text-gradient",
+			},
+			{
+				name: "redux",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "rtk query",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "restapi",
+				color: "green-text-gradient",
+			},
+			{
+				name: "node-cache",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "express",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "mongodb",
+				color: "green-text-gradient",
+			},
+			{
+				name: "scss",
+				color: "pink-text-gradient",
+			},
+		],
+		image: jobit,
+		source_code_link:
+			"https://github.com/DavidGoyal/MERN-ECommerce-FRONTEND.git",
+		deployed_link: "https://mern-e-commerce-frontend-seven.vercel.app",
+	},
+	{
+		name: "XCrypto",
+		description:
+			"A crypto tracker website helping users to get realtime data about different crypto currencies and exchanges",
+		tags: [
+			{
+				name: "react",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "chakra-ui",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "framer-motion",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "chartjs",
+				color: "pink-text-gradient",
+			},
+			{
+				name: "react-icons",
+				color: "pink-text-gradient",
+			},
+		],
+		image: tripguide,
+		source_code_link: "https://github.com/DavidGoyal/Crypto_App",
+		deployed_link: "https://crypto-app-tan-ten.vercel.app",
+	},
+	{
+		name: "Netflix Clone",
+		description: "UI clone of Netflix made using html and css",
+		tags: [
+			{
+				name: "html",
+				color: "blue-text-gradient",
+			},
+			{
+				name: "css",
+				color: "pink-text-gradient",
+			},
+		],
+		image: netflix,
+		source_code_link: "https://github.com/DavidGoyal/Netflix-UI-Clone",
+	},
+];
+
+export { services, technologies, projects };
